fd931b796608d7b421886d224b094cd27d5b7d8e
Helladox <koreanserverff444@gmail.com>
2025-04-27 21:00:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
-auth_users = [7991495621]
+api_id = "20723624"
+api_hash = "6b2d857266669bab971257d8312af741"
+bot_token = "7827643836:AAGw1pSJgVYk-kyvCTYH6_ep0OHKiyldUeM"
+auth_users = [7777851415]
